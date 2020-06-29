570fde6d5463d646fd591bddca9557e974a18301
zhouhaolei <yeyan1996@gmail.com>
2020-06-29 13:53:10 +0800

    test: docker 测试

--- index.js
+++ index.js
@@ -49,17 +49,17 @@ http.createServer(async (req, res) => {
         fs.copyFileSync(path.resolve(__dirname,`./.dockerignore`), path.resolve(projectDir, './.dockerignore'))
 
 
-        // // 创建 docker 镜像
-        // execSync(`docker build -t yeyan1996/${data.repository.name}-image:latest .`, {
-        //     stdio: 'inherit',
-        //     cwd: projectDir
-        // })
-
         // 创建 docker 镜像
-        execSync(`docker pull yeyan1996/docker-test-image:latest`, {
+        execSync(`docker build -t yeyan1996/${data.repository.name}-image:latest .`, {
             stdio: 'inherit',
             cwd: projectDir
         })
+
+        // // 创建 docker 镜像
+        // execSync(`docker pull yeyan1996/docker-test-image:latest`, {
+        //     stdio: 'inherit',
+        //     cwd: projectDir
+        // })
 
         // 销毁 docker 容器
         execSync(`docker ps -a -f "name=^${data.repository.name}-container" --format="{{.ID}}" | xargs -r docker stop | xargs -r docker rm`, {
